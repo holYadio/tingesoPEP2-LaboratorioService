47319be0e1630bc9f6085fb83eb67c05d00c9011
Jhoisan Allendes <jhoisan.allendes@usach.cl>
2023-06-13 18:49:54 -0400

    fix: bootstrap.yaml

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO laboratorio(proveedor,porcentaje_grasa,porcentaje_solido_total,quincena) VALUES ('01001','25','24','2023/03/Q2');
+INSERT INTO laboratorio(proveedor,porcentaje_grasa,porcentaje_solido_total,quincena) VALUES ('01002','08','11','2023/03/Q2');
+INSERT INTO laboratorio(proveedor,porcentaje_grasa,porcentaje_solido_total,quincena) VALUES ('01003','30','40','2023/03/Q2');
+INSERT INTO laboratorio(proveedor,porcentaje_grasa,porcentaje_solido_total,quincena) VALUES ('02004','40','27','2023/03/Q2');
+INSERT INTO laboratorio(proveedor,porcentaje_grasa,porcentaje_solido_total,quincena) VALUES ('01001','25','14','2023/03/Q1');
+INSERT INTO laboratorio(proveedor,porcentaje_grasa,porcentaje_solido_total,quincena) VALUES ('01002','08','11','2023/03/Q1');
+INSERT INTO laboratorio(proveedor,porcentaje_grasa,porcentaje_solido_total,quincena) VALUES ('01003','30','50','2023/03/Q1');
+INSERT INTO laboratorio(proveedor,porcentaje_grasa,porcentaje_solido_total,quincena) VALUES ('02004','40','27','2023/03/Q1');
